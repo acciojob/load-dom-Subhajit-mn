142ed354310fb5987d295e8f11ad74f95f70e934
Subhajit Mandal <108950928+Subhajit-mn@users.noreply.github.com>
2023-09-19 23:13:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //your JS code here. If required.
-function loading(){
+// function loading(){
+// 	document.body.innerText = "DOM load success";
+// }
+document.addEventListner("DOMContentLoaded", ()=>{
 	document.body.innerText = "DOM load success";
-}
-//docume.addEventListner("DOMContentLoaded", onDOMLoad);
+});
 
 //document.getElementByTagName("p");
